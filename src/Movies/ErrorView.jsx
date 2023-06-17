2c56de61cb78a78f90db5de5e3f11615a727396e
“tcrz” <tonybaidoo0@gmail.com>
2023-06-17 18:43:47 +0000

    style updates

--- src/Movies/ErrorView.jsx
+++ src/Movies/ErrorView.jsx
@@ -6,7 +6,7 @@ const ErrorView = ({refetch}) => {
     <div className="flex flex-col items-center justify-around" style={{height: "80vh"}}>
       <div className="borderr-2 border-black flex flex-col items-center">
         <img className="" src={logo} alt="moviefind logo" style={{width:"50%", height: "50%"}}/>
-        <p className="text-xl text-red-500">Sorry, an error occurred. <span className="text-gray-500 underline cursor-pointer" onClick={refetch}>Try again</span></p>
+        <p className="text-center text-xl text-red-500">Sorry, an error occurred. <span className="text-gray-500 underline cursor-pointer" onClick={refetch}>Try again</span></p>
       </div>
       
     </div>
--- src/Movies/LoadingView.jsx
+++ src/Movies/LoadingView.jsx
@@ -4,7 +4,7 @@ import { CircularProgress } from '@mui/material'
 
 const LoadingView = () => {
   return (
-    <div className="flex flex-col items-center justify-around" style={{height: "80vh"}}>
+    <div className=" flex flex-col items-center justify-around" style={{height: "80vh"}}>
       <div className="borderr-2 border-black flex flex-col items-center">
         <CircularProgress />
         <p className="text-xl text-gray-500">Loading results...</p>
