c98c1327d74ade80fceb7b4ddf72047a955ad0b9
“tcrz” <tonybaidoo0@gmail.com>
2023-06-17 11:46:34 +0000

    handle various scenarios based on state of data fetching

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -6,25 +6,22 @@ import ErrorView from './ErrorView'
 import LoadingView from './LoadingView';
 
 
-const Movies = ({numOfPages, pageNum, movies, isLoading, isError, isSuccess, refetch}) => {
+const Movies = ({movies, isLoading, isFetching, isError, isSuccess, refetch}) => {
+
   return (
     <>
-      <body className="borderr-2 border-black bg-gray-200/80" style={{ marginTop: "9vh", height: "90vh" }}>
-          {
-            isSuccess && 
-            <section style={{ height: "80vh" }} className="borderr-2 pokemon-list grid grid-cols-1 gap-x-4 gap-y-4 px-20 pt-5 h-full overflow-y-scroll sm:grid-cols-2 lg:grid-cols-4">
-              {movies.map((movie) => <MovieCard title={movie.Title} year={movie.Year} poster={movie.Poster} />)}
-            </section>
-          }
-        <section style={{ height: "80vh" }} className="borderr-2 pokemon-list grid grid-cols-1 gap-x-4 gap-y-4 px-20 pt-5 h-full overflow-y-scroll sm:grid-cols-2 lg:grid-cols-4">
-          <MovieCard />
-        </section>
-        {isError && <ErrorView />}
-        {isSuccess && movies.length === 0 && <EmptyView />}
-        {isLoading && <LoadingView />}
+      <section className="borderr-2 border-black bg-gray-200/80" style={{ marginTop: "9vh", height: "80vh" }}>
+        {isError && <ErrorView refetch={refetch} />}
+        {(isLoading || isFetching) && <LoadingView />}
+        {(!isLoading && !isFetching) && isSuccess && movies.length === 0 && <EmptyView />}
         {movies === null && <DefaultView />}
-        
-      </body>
+        {
+          (!isLoading && !isFetching) && isSuccess && 
+          <div style={{ height: "80vh" }} className="borderr-2 pokemon-list grid grid-cols-1 gap-x-4 gap-y-4 px-20 pt-5 h-full overflow-y-scroll sm:grid-cols-2 lg:grid-cols-4">
+            {movies.map((movie) => <MovieCard key={movie.imdbID} title={movie.Title} year={movie.Year} poster={movie.Poster} />)}
+          </div>
+        }
+      </section>
     </>
   )
 }
