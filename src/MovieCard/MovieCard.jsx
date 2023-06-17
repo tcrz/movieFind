1ce351f77f6bc71c8c564f163da21b0135b5db6b
“tcrz” <tonybaidoo0@gmail.com>
2023-06-17 11:44:30 +0000

    pass props to component

--- src/MovieCard/MovieCard.jsx
+++ src/MovieCard/MovieCard.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { FaEye } from "react-icons/fa";
 import "./MovieCard.css"
+import defaultPoster from "./defaultPoster.png"
 
 const MovieCard = ({title, year, poster}) => {
-  const name = "The Shinning of Jonathan Ansah Boateng"
   return (
     <article className="movie bg-red-400 borderr-4 border-orange-500 flex flex-col items-center relative" style={{ height: "280px" }}>
       <div className="inner-card-container w-full h-full bgg-red-900 flex flex-col items-center relative" style={{width: "100%"}}>
         <div className="inner-card border border-white w-full h-full p-2 flex flex-col gap-4 bgg-black" style={{ marginBottomm: "5%", width: "100%" }}>
           <div className="inner-card-bg borderr border-black w-full flex flex-col items-center" style={{ marginToop: "25%", height: "65%", width: "100%" }}>
-            <img className="" src="https://upload.wikimedia.org/wikipedia/en/8/84/Moonlight_%282016_film%29.png" alt="poster" style={{height: "100%", width: "100%"}}/>
+            <img className="" src={poster !== "N/A" ? poster : defaultPoster} alt="poster" style={{height: "100%", width: "100%"}}/>
           </div>
           <div className="borderr border-blue-900 flex flex-col gap-3 items-center " style={{ height: "30%" }}>
-            <p className="title text-xl font-medium w-full" >{name}</p>
+            <p className="title text-xl font-medium w-full text-center" >{title}</p>
             <div className="borderr flex flex-wrap justify-around items-center gap-5" style={{ minWidth: "55%" }}>
               <p className="text-sm rounded-xl bg-gray-200 px-2" style={{ background: "#EEEEEE" }}>{2023}</p>
             </div>
